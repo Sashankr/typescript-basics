db349222ad9c720f9c047e9fcc1e49dee8af2df9
Sashankr <sashankr97@gmail.com>
2025-02-10 00:03:52 +0530

    defining types for object with type alias

--- app.ts
+++ app.ts
@@ -14,3 +14,14 @@ function add1(input1: Combined, input2: Combined, resultOutput: OutputType) {
 add1(1, 2, "as-number");
 add1("1", "2", "as-number");
 // add("1", "2", "as-numb");// error: as-numb cannot be assigned to type as-text|as-number
+
+type User = {
+  name: string;
+  role: string;
+};
+
+function greetUser(user: User) {
+  console.log(`Hello ${user.name} your role is ${user.role}`);
+}
+
+greetUser({ name: "Sashank", role: "Developer" });
