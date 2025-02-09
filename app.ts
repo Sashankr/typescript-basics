a70ac1357a35a6e8b65d0519e8dd531d3d53d75a
Sashankr <sashankr97@gmail.com>
2025-02-09 23:19:27 +0530

    literal types

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
-function add(input1: number | string, input2: number | string) {
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    return input1 + input2;
+function add(
+  input1: number | string,
+  input2: number | string,
+  resultOutput: "as-text" | "as-number"
+) {
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultOutput === "as-number"
+  ) {
+    return +input1 + +input2;
   } else {
     return input1.toString() + input2.toString();
   }
 }
-add(1, 2);
-add("1", "2");
+add(1, 2, "as-number");
+add("1", "2", "as-number");
+// add("1", "2", "as-numb");// error: as-numb cannot be assigned to type as-text|as-number
